48d65ac7e53f32e09792dad8fdfd51e231675c5e
Robert <robertmaximus383@gmail.com>
2023-03-20 12:52:35 +0200

    Wallet Balance Logic & Header in progress

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import { WalletBalanceProvider } from '../context/useWalletBalance'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WalletBalanceProvider>
+          <Component {...pageProps} />
+    </WalletBalanceProvider>
+  )
 }
 
 export default MyApp

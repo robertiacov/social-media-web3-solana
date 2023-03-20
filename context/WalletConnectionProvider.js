395d4e82d4aaeb55a73616b6cbbf642f80a72379
Robert <robertmaximus383@gmail.com>
2023-03-20 13:57:56 +0200

    Wallet Connection Provider bug fix

--- context/WalletConnectionProvider.js
+++ context/WalletConnectionProvider.js
@@ -1,21 +1,24 @@
-import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
-import { useMemo } from "react";
-
-const WalletConnectionProvider = ({childeren}) => {
-    const endpoint = useMemo(() => "https://api.devnet.solana.com", []);
-
-    const wallets = useMemo(
-        () => [ new PhantomWalletAdapter()], []);
-
+import {
+    ConnectionProvider,
+    WalletProvider,
+  } from '@solana/wallet-adapter-react'
+  import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+  import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets'
+  import { useMemo } from 'react'
+  
+  
+  const WalletConnectionProvider = ({ children }) => {
+    const endpoint = useMemo(() => 'https://api.devnet.solana.com', [])
+  
+    const wallets = useMemo(() => [new PhantomWalletAdapter()], [])
+  
     return (
-        <ConnectionProvider endpoint={endpoint}>
-            <WalletProvider wallets={wallets} autoConnect>
-                <WalletModalProvider>{childeren}</WalletModalProvider>
-            </WalletProvider>
-        </ConnectionProvider>
-    );
-}
-
-export default WalletConnectionProvider;
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+          <WalletModalProvider>{children}</WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    )
+  }
+  
+  export default WalletConnectionProvider

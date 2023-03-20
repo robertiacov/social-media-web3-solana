817fc146550bb0a7c24e89784f12c61771968dff
Robert <robertmaximus383@gmail.com>
2023-03-20 13:53:21 +0200

    Wallet Connection Provider and App wrap around

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
 import '../styles/globals.css'
 import { WalletBalanceProvider } from '../context/useWalletBalance'
+import dynamic from 'next/dynamic'
 
 function MyApp({ Component, pageProps }) {
+
+  const WalletConnectionProvider = dynamic(
+    () => import('../context/WalletConnectionProvider'),
+    { ssr: false },
+  )
+
+
   return (
-    <WalletBalanceProvider>
-          <Component {...pageProps} />
-    </WalletBalanceProvider>
+      <WalletConnectionProvider>
+          <WalletBalanceProvider>
+                <Component {...pageProps} />
+          </WalletBalanceProvider>
+      </WalletConnectionProvider>
   )
 }
 

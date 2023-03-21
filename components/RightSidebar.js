d127a719f5a2268e12b879c79e8c1498004d75ae
Robert <robertmaximus383@gmail.com>
2023-03-21 20:12:46 +0200

    Left sidebar created and ads set

--- components/RightSidebar.js
+++ components/RightSidebar.js
@@ -1,8 +1,51 @@
 import React from 'react'
+import Image from 'next/image'
+import Contact from './Contact'
 
 const RightSidebar = () => {
   return (
-    <div>RightSidebar</div>
+    <div className={style.wrapper}>
+        <div className={style.title}>Sponsored</div>
+        <div className={style.adsContainer}>
+            <div className={style.ad}>
+                <div className={style.adImageContainer}>
+                    <Image 
+                    src={
+                        'https://images.unsplash.com/photo-1616489798881-8e1b0e1b2e1c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+                    }
+                    height={100}
+                    width={100}
+                    className = {style.adImage}
+                    alt = 'ad logo'
+                    />
+                </div>
+                <div>
+                    <div>This is a nice ad picture.</div>
+                    <div className={style.adLink}>https://www.adPicture.com</div>
+                </div>
+            </div>
+            <div className={style.ad}>
+                <div className={style.adImageContainer}>
+                    <Image 
+                    src = {
+                        'https://images.unsplash.com/photo-1616489798881-8e1b0e1b2e1c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+                    }
+                    height={100}
+                    width={100}
+                    className = {style.adImage}
+                    alt = 'bad ad logo'
+                    />
+                </div>
+                <div>
+                    <div>Not so nice ad.</div>
+                    <div className={style.adLink}>https://www.badAd.com</div>
+                </div>
+            </div>
+            <div className={style.divider}/>
+            <div className={style.title}>Contacts</div>
+            
+        </div>
+    </div>
   )
 }
 

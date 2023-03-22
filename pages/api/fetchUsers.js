e50d6a34b9430b9d0f78b110c9cfa3490dbe9c7f
Robert <robertmaximus383@gmail.com>
2023-03-22 12:57:27 +0200

    fetch the user info into client response for Sanity

--- pages/api/fetchUsers.js
+++ pages/api/fetchUsers.js
@@ -9,6 +9,10 @@ const getUserInfo = async (req, res) => {
             profileImage,
         }
         `
+        const sanityResponse = await client.fetch(query)
+
+        res.status(200).send({message: 'success', data:sanityResponse})
+
     } catch (error) {
         res.status(500).send({message: 'error', data:error.message})
     }

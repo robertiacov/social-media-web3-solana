b351b94823f6f9111489b3c5f13e2fa532c09f46
Robert <robertmaximus383@gmail.com>
2023-03-22 12:05:02 +0200

    fetch user API done

--- pages/api/fetchUsers.js
+++ pages/api/fetchUsers.js
@@ -2,8 +2,16 @@ import {client} from '../../lib/sanity'
 
 const getUserInfo = async (req, res) => {
     try {
-        
+        const query = `
+        *[_type == "users"]{
+            name,
+            walletAddress,
+            profileImage,
+        }
+        `
     } catch (error) {
         res.status(500).send({message: 'error', data:error.message})
     }
 }
+
+export default getUserInfo

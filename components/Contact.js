be3de051418c4c2a41bbc3e4cbcdc6a1239f68c0
Robert <robertmaximus383@gmail.com>
2023-03-22 11:49:02 +0200

    Contact component design

--- components/Contact.js
+++ components/Contact.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import Image from 'next/image'
 
-const Contact = () => {
+const Contact = ({user}) => {
+
+    const style = {
+        contact: `flex items-center my-2`,
+        contactImage: `rounded-full object-cover`,
+        contactName: `ml-4 text-[1rem]`,
+    }
+
   return (
-    <div>Contact</div>
+    <div className={style.contact}>
+        <Image 
+        src={user.profileImage}
+        height={40}
+        width={40}
+        className = {style.contactImage}
+        />
+        <div className={style.contactName}>{user.name}</div>
+    </div>
   )
 }
 

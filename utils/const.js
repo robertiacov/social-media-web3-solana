754bab1db3b6239e511a3962532331a1bb8ba586
Robert <robertmaximus383@gmail.com>
2023-03-25 19:36:23 +0200

    smart contract id updated for front-end

--- utils/const.js
+++ utils/const.js
@@ -4,6 +4,6 @@ import web3_solana from './web3_solana.json'
 export const CLUSTER = 'devnet';
 export const SOLANA_HOST = 'https://api.devnet.solana.com';
 
-export const STABLE_POOL_PROGRAM_ID = new PublicKey('25akBwuj7r67QenKtB7sJytKYuC1XBVagrS3dbmMuEXf');
+export const STABLE_POOL_PROGRAM_ID = new PublicKey('8fqd59rTXPE6P1RgekNGPSj4kUEaxZAhx3DaxRrBoCRh');
 
 export const STABLE_POOL_IDL = web3_solana

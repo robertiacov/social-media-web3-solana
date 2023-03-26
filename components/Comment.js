7141b3871581bde8d35099f46d3d5ffe4a68f600
Robert <robertmaximus383@gmail.com>
2023-03-26 20:35:30 +0300

    TimeAgo bug solved

--- components/Comment.js
+++ components/Comment.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import TimeAgo from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en.json'
 
-TimeAgo.addDefaultLocale(en)
+TimeAgo.addLocale(en)
 
 const timeAgo = new TimeAgo('en-US')
 

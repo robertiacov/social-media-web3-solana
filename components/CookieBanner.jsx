99686cf5241ba73933178ee073dea38c7a2cbc97
Robert <robertmaximus383@gmail.com>
2023-06-15 22:04:41 +0300

    Cookie banner desing added

--- components/CookieBanner.jsx
+++ components/CookieBanner.jsx
@@ -33,10 +33,10 @@ export default function CookieBanner(){
     return (
         <div className={`my-10 mx-auto max-w-max md:max-w-screen-sm
                         fixed bottom-0 left-0 right-0 
-                        ${cookieConsent != null ? "hidden" : "flex"} px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4  
-                         bg-gray-700 rounded-lg shadow`}>
+                        flex px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4  
+                        bg-gray-700 rounded-lg shadow`}>
 
-            <div className='text-center'>
+            <div className='text-center text-white'>
                 <Link href="/info/cookies"><p>We use <span className='font-bold text-sky-400'>cookies</span> on our site.</p></Link>
             </div>
 
